77bc0107eedb2c10d2b6a0a8b9f1a27c92ae8eb8
Pio <pio.cho.bia@gmail.com>
2024-01-31 14:46:46 +0100

    Dodaj pliki projektów.

---
+++ Mob_240131_Oplaty/Mob_240131_Oplaty/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Mob_240131_Oplaty.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mob_240131_Oplaty
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new Suma();
+    }
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnSleep()
+    {
+    }
+
+    protected override void OnResume()
+    {
+    }
+  }
+}
---
+++ Mob_240131_Oplaty/Mob_240131_Oplaty/Views/Suma.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mob_240131_Oplaty.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class Suma : TabbedPage
+  {
+    public Suma()
+    {
+      var prad = new NavigationPage(new Prad()) { Title = "prad" };
+      var gaz = new NavigationPage(new Gaz()) { Title = "gaz" };
+      var zw = new NavigationPage(new ZimnaWoda()) { Title = "zw" };
+      var cw = new NavigationPage(new CieplaWoda()) { Title = "cw" };
+      Children.Add(prad);
+      Children.Add(gaz);
+      Children.Add(zw);
+      Children.Add(cw);
+
+      InitializeComponent();
+    }
+  }
+}
